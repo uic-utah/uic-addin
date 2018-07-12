f3e69f9a2ad6bc390400c27ed11161a14499025d
steveoh <sgourley@utah.gov>
2018-07-12 12:49:04 -0600

    add queue task stuff to thread service

--- Services/ThreadService.cs
+++ Services/ThreadService.cs
@@ -1,13 +1,15 @@
 using System;
+using System.Threading.Tasks;
 using System.Windows;
 using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
 
 namespace uic_addin.Services {
     public class ThreadService {
         public static bool IsOnUiThread => FrameworkApplication.TestMode ||
                                            Application.Current.Dispatcher.CheckAccess();
 
-        internal static void RunOnUiThread(Action action) {
+        public static void RunOnUiThread(Action action) {
             try {
                 if (IsOnUiThread) {
                     action();
@@ -17,5 +19,9 @@ namespace uic_addin.Services {
             } catch (Exception) {
             }
         }
+
+        public static Task RunOnBackground(Action action) => QueuedTask.Run(action);
+
+        public static Task<T> RunOnBackground<T>(Func<T> func) => QueuedTask.Run(func);
     }
 }
