76cbbb9a888d22981635a909c55a7601c92b890a
steveoh <sgourley@utah.gov>
2018-07-11 20:00:10 -0600

    rename properities and add validation attributes

--- Models/FacilityModel.cs
+++ Models/FacilityModel.cs
@@ -1,12 +1,13 @@
-using System;
-using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
-using ArcGIS.Desktop.Framework.Contracts;
+using System.ComponentModel.DataAnnotations;
 using ArcGIS.Desktop.Mapping;
-using Microsoft.Build.Framework;
+using uic_addin.Attributes;
+using uic_addin.ViewModels;
 
 namespace uic_addin.Models {
-    public class FacilityModel : PropertyChangedBase, INotifyDataErrorInfo {
+    public class FacilityModel : PropertyChangedWithValidation {
         public const string IdField = "FacilityID";
         public const string TableName = "UicFacility";
         public readonly FeatureLayer FeatureLayer;
@@ -21,7 +22,7 @@ namespace uic_addin.Models {
         private string _facilityState;
         private string _facilityZip;
         private string _naicsPrimary;
-        private string _uicFacilityId = "";
+        private string _facilityId = "";
 
         private FacilityModel() {
         }
@@ -32,49 +33,60 @@ namespace uic_addin.Models {
 
         public long SelectedOid { get; set; }
 
-        [Required]
-        public string UicFacilityId {
-            get => _uicFacilityId;
-            set => SetProperty(ref _uicFacilityId, value);
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(14, ErrorMessage = "Facility Identifiers contain 14 characters")]
+        [DisplayName("Facility Id")]
+        public string FacilityId {
+            get => _facilityId;
+            set => SetProperty(ref _facilityId, value);
         }
 
         [Required]
+        [DisplayName("Guid")]
         public string FacilityGuid {
             get => _facilityGuid;
             set => SetProperty(ref _facilityGuid, value);
         }
 
         [Required]
+        [FipsCode]
+        [DisplayName("County FIPS")]
         public string CountyFips {
             get => _countyFips;
             set => SetProperty(ref _countyFips, value);
         }
 
         [Required]
+        [StringLength(6, ErrorMessage = "NIACS codes are 1-6 characters")]
+        [DisplayName("NAICS Code")]
         public string NaicsPrimary {
             get => _naicsPrimary;
             set => SetProperty(ref _naicsPrimary, value);
         }
 
-        [Required]
+        [Required(AllowEmptyStrings = false)]
+        [DisplayName("Facility Name")]
         public string FacilityName {
             get => _facilityName;
             set => SetProperty(ref _facilityName, value);
         }
 
-        [Required]
+        [Required(AllowEmptyStrings = false)]
+        [DisplayName("Facility Address")]
         public string FacilityAddress {
             get => _facilityAddress;
             set => SetProperty(ref _facilityAddress, value);
         }
 
-        [Required]
+        [Required(AllowEmptyStrings = false)]
+        [DisplayName("Facility City")]
         public string FacilityCity {
             get => _facilityCity;
             set => SetProperty(ref _facilityCity, value);
         }
 
         [Required]
+        [DisplayName("Facility State")]
         public string FacilityState {
             get => _facilityState;
             set => SetProperty(ref _facilityState, value);
@@ -95,11 +107,5 @@ namespace uic_addin.Models {
             set => SetProperty(ref _comments, value);
         }
 
-        public IEnumerable GetErrors(string propertyName) {
-            throw new NotImplementedException();
-        }
-
-        public bool HasErrors { get; }
-        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
     }
 }
