bb785cce40f27f3765ec1ab36e08c2f46f59af49
steveoh <sgourley@utah.gov>
2018-07-16 20:26:32 -0600

    :warning: create pane to create new facility

---
+++ Controls/CreateNewFacilityPaneOpenButton.cs
@@ -0,0 +1,13 @@
+﻿using ArcGIS.Desktop.Framework.Contracts;
+
+namespace uic_addin.Controls
+{
+    /// <summary>
+    ///     Button implementation to create a new instance of the pane and activate it.
+    /// </summary>
+    internal class CreateNewFacilityPaneOpenButton : Button {
+        protected override void OnClick() {
+            CreateNewFacilityPaneViewModel.Create();
+        }
+    }
+}
