a619dee623c32cf1ba934aa1730eb19e9c66bb0f
steveoh <sgourley@utah.gov>
2018-07-23 13:43:14 -0600

    switch to map impersonation pane

--- Views/WorkflowViewModel.cs
+++ Views/WorkflowViewModel.cs
@@ -5,9 +5,9 @@ using System.Reactive.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using ArcGIS.Core.CIM;
 using ArcGIS.Core.Events;
 using ArcGIS.Desktop.Framework;
-using ArcGIS.Desktop.Framework.Contracts;
 using ArcGIS.Desktop.Mapping;
 using ArcGIS.Desktop.Mapping.Events;
 using Reactive.Bindings;
@@ -17,12 +17,16 @@ using uic_addin.Models;
 using uic_addin.Services;
 
 namespace uic_addin.Views {
-    internal class WorkflowDockPaneViewModel : DockPane {
-        public const string DockPaneId = "WorkflowDockPane";
-        private readonly SubscriptionToken _token;
-        private string _heading = "UIC Workflow Main";
+    internal class WorkflowViewModel : TOCMapPaneProviderPane {
+        private const string ViewPaneId = "WorkflowPane";
+        private readonly SubscriptionToken _subscriptionToken;
 
-        public WorkflowDockPaneViewModel() {
+        /// <summary>
+        ///     Consume the passed in CIMView. Call the base constructor to wire up the CIMView.
+        /// </summary>
+        public WorkflowViewModel(CIMView view) : base(view) {
+            _dockUnderMapView = true;
+
             UpdateToVersionMessage = UicModule.Current.IsCurrent.Select(x => {
                                                   if (x) {
                                                       return "Your add-in is up to date! 💙";
@@ -72,54 +76,12 @@ namespace uic_addin.Views {
                 }
             }, () => MapView.Active?.Map.SelectionCount > 0);
 
-            if (MapView.Active == null) {
-                _token = MapViewInitializedEvent.Subscribe(args => SetupMapSubscriptions(args.MapView.Map));
-                return;
-            }
-
-            SetupMapSubscriptions(MapView.Active.Map);
-        }
-
-        public string Heading {
-            get => _heading;
-            set => SetProperty(ref _heading, value, () => Heading);
-        }
-
-        public ICommand UseSelection { get; }
-
-        public ICommand ZoomToFacility { get; }
-
-        public ICommand ShowAttributeEditorForSelectedRecord { get; }
-
-        public ICommand ShowSettings => FrameworkApplication.GetPlugInWrapper("esri_core_showOptionsSheetButton") as
-            ICommand;
-
-        // Exposed Model
-        public ReactiveProperty<FacilityModel> Model { get; set; } =
-            new ReactiveProperty<FacilityModel>(mode: ReactivePropertyMode.DistinctUntilChanged);
-
-        public ReactiveProperty<string> FacilityId { get; set; } =
-            new ReactiveProperty<string>(mode: ReactivePropertyMode.DistinctUntilChanged);
-
-        public ReactiveProperty<IEnumerable<string>> Facilities { get; set; }
-
-        public ReactiveProperty<bool> ShowUpdate { get; set; }
-
-        public ReactiveProperty<string> UpdateToVersionMessage { get; set; }
-
-        public ReactiveCommand UpdateSelf { get; set; } = new ReactiveCommand();
-
-        public void SetupMapSubscriptions(Map map) {
-            if (_token != null) {
-                MapViewInitializedEvent.Unsubscribe(_token);
-            }
-
             Facilities = FacilityId.Select(async id => {
                                        if (id.Length < 4) {
                                            return Enumerable.Empty<string>();
                                        }
 
-                                       return await QueryService.GetFacilityIdsFor(id, map);
+                                       return await QueryService.GetFacilityIdsFor(id, MapView.Active.Map);
                                    })
                                    .CatchIgnore()
                                    .Switch()
@@ -145,7 +107,7 @@ namespace uic_addin.Views {
                           await LayerService.SetSelectionFromId(Model.Value.ObjectId, facilityLayer);
                       });
 
-            MapSelectionChangedEvent.Subscribe(async args => {
+            _subscriptionToken = MapSelectionChangedEvent.Subscribe(async args => {
                 if (!FrameworkApplication.State.Contains(UicModule.Current.FacilitySelectionState)) {
                     return;
                 }
@@ -174,9 +136,42 @@ namespace uic_addin.Views {
             });
         }
 
-        public static void Show() {
-            var pane = FrameworkApplication.DockPaneManager.Find(DockPaneId);
-            pane?.Activate();
+        public RelayCommand UseSelection { get; }
+
+        public RelayCommand ZoomToFacility { get; }
+
+        public RelayCommand ShowAttributeEditorForSelectedRecord { get; }
+
+        public ICommand ShowSettings => FrameworkApplication.GetPlugInWrapper("esri_core_showOptionsSheetButton") as
+            ICommand;
+
+        // Exposed Model
+        public ReactiveProperty<FacilityModel> Model { get; set; } =
+            new ReactiveProperty<FacilityModel>(mode: ReactivePropertyMode.DistinctUntilChanged);
+
+        public ReactiveProperty<string> FacilityId { get; set; } =
+            new ReactiveProperty<string>(mode: ReactivePropertyMode.DistinctUntilChanged);
+
+        public ReactiveProperty<IEnumerable<string>> Facilities { get; set; }
+
+        public ReactiveProperty<bool> ShowUpdate { get; set; }
+
+        public ReactiveProperty<string> UpdateToVersionMessage { get; set; }
+
+        public ReactiveCommand UpdateSelf { get; set; } = new ReactiveCommand();
+
+        /// <summary>
+        ///     Must be overridden in child classes used to persist the state of the view to the CIM.
+        /// </summary>
+        /// <remarks>View state is called on each project save</remarks>
+        public override CIMView ViewState {
+            get {
+                _cimView.InstanceID = (int)InstanceID;
+                //Cache content in _cimView.ViewProperties or in _cimView.ViewXML
+                //_cimView.ViewXML = new XDocument(new XElement("Root",
+                //new XElement("custom", "custom value"))).ToString(SaveOptions.DisableFormatting);
+                return _cimView;
+            }
         }
 
         private static async Task UpdateAddin() {
@@ -186,13 +181,58 @@ namespace uic_addin.Views {
 
             await UicModule.Current.Evergreen.Value.Update(UicModule.Current.EvergreenSettings.LatestRelease);
 
-            var notification = new Notification {
-                Message = "Restart to complete the update.",
-                ImageUrl = "",
-                Title = "Evergreen: Restart Required"
+            var result = MessageBox.Show("A restart is required to complete the update. Would you like to exit Pro now?", "Evergreen: Restart Required",
+                            MessageBoxButton.YesNo);
+
+            if (result == MessageBoxResult.Yes) {
+                await FrameworkApplication.ShutdownAsync();
+            }
+        }
+
+        /// <summary>
+        ///     Create a new instance of the pane.
+        /// </summary>
+        internal static WorkflowViewModel Create(MapView mapView) {
+            var view = new CIMGenericView {
+                ViewType = ViewPaneId,
+                ViewProperties = new Dictionary<string, object>()
             };
 
-            FrameworkApplication.AddNotification(notification);
+            view.ViewProperties["MAPURI"] = mapView.Map.URI;
+
+            if (!(FrameworkApplication.Panes.Create(ViewPaneId, view) is WorkflowViewModel newPane)) {
+                return null;
+            }
+
+            newPane.Caption = "UIC Workflow";
+
+            return newPane;
+        }
+
+        /// <summary>
+        ///     Called when the pane is initialized.
+        /// </summary>
+        protected override async Task InitializeAsync() {
+            var uri = ((CIMGenericView)_cimView).ViewProperties["MAPURI"] as string;
+            await SetMapURI(uri);
+
+            await base.InitializeAsync();
+        }
+
+        /// <summary>
+        ///     Called when the pane is uninitialized.
+        /// </summary>
+        protected override async Task UninitializeAsync() {
+            MapSelectionChangedEvent.Unsubscribe(_subscriptionToken);
+
+            Facilities.Dispose();
+            FacilityId.Dispose();
+            Model.Dispose();
+            ShowUpdate.Dispose();
+            UpdateToVersionMessage.Dispose();
+            UpdateSelf.Dispose();
+
+            await base.UninitializeAsync();
         }
     }
 }
