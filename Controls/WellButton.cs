750e963f5f75cde792c8a4cceec2f6bd8d715577
steveoh <sgourley@utah.gov>
2019-09-03 14:59:13 -0600

    chore: mock up new ribbon UI
    refs #25

---
+++ Controls/WellButton.cs
@@ -0,0 +1,11 @@
+using ArcGIS.Desktop.Framework.Contracts;
+
+namespace uic_addin.Controls {
+    internal class WellButton : Button {
+        
+    }
+
+    internal class AoRButton : Button {
+
+    }
+}
