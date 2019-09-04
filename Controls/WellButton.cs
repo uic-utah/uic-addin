900ae3155e2b37d2293e5c786d5b651d550fde9c
steveoh <sgourley@utah.gov>
2019-09-04 10:34:34 -0600

    fix: denest settings and update buttons

--- Controls/WellButton.cs
+++ Controls/WellButton.cs
@@ -71,35 +71,36 @@ namespace uic_addin.Controls {
             });
         });
 
-        internal class AoRButton : Button {
+    }
 
+    internal class AoRButton : Button {
+
+    }
+
+    internal class PropertiesButton : Button {
+        protected override void OnClick() => PropertySheet.ShowDialog("esri_core_optionsPropertySheet", "EvergreenSettings");
+    }
+
+    internal class UpdateButton : Button {
+        public UpdateButton() {
+            Caption = UicModule.Current.IsCurrent.Value ? "Up to date! 💙" : $"Update to {UicModule.Current.EvergreenSettings?.LatestRelease?.TagName}";
+            Tooltip = UicModule.Current.EvergreenSettings?.LatestRelease?.TagName;
         }
 
-        internal class PropertiesButton : Button {
-            protected override void OnClick() => PropertySheet.ShowDialog("esri_core_optionsPropertySheet", "EvergreenSettings");
-        }
-
-        internal class UpdateButton : Button {
-            public UpdateButton() {
-                Caption = UicModule.Current.IsCurrent.Value ? "Up to date! 💙" : $"Update to {UicModule.Current.EvergreenSettings?.LatestRelease?.TagName}";
-                Tooltip = UicModule.Current.EvergreenSettings?.LatestRelease?.TagName;
+        protected override async void OnClick() {
+            if (UicModule.Current.EvergreenSettings.LatestRelease == null) {
+                return;
             }
 
-            protected override async void OnClick() {
-                if (UicModule.Current.EvergreenSettings.LatestRelease == null) {
-                    return;
-                }
+            await UicModule.Current.Evergreen.Value.Update(UicModule.Current.EvergreenSettings.LatestRelease);
 
-                await UicModule.Current.Evergreen.Value.Update(UicModule.Current.EvergreenSettings.LatestRelease);
+            var result =
+                MessageBox.Show("A restart is required to complete the update. Would you like to exit Pro now?",
+                                "Evergreen: Restart Required",
+                                MessageBoxButton.YesNo);
 
-                var result =
-                    MessageBox.Show("A restart is required to complete the update. Would you like to exit Pro now?",
-                                    "Evergreen: Restart Required",
-                                    MessageBoxButton.YesNo);
-
-                if (result == MessageBoxResult.Yes) {
-                    await FrameworkApplication.ShutdownAsync();
-                }
+            if (result == MessageBoxResult.Yes) {
+                await FrameworkApplication.ShutdownAsync();
             }
         }
     }
