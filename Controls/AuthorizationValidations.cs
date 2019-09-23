db5c814216172518afa79d5de93951e2fa95c0eb
steveoh <sgourley@utah.gov>
2019-09-23 17:49:44 -0600

    refactor(authorizatons): add progress dialog and use notificationservice

--- Controls/AuthorizationValidations.cs
+++ Controls/AuthorizationValidations.cs
@@ -11,11 +11,15 @@ using ArcGIS.Desktop.Mapping;
 using ArcGIS.Desktop.Framework;
 using uic_addin.Extensions;
 using ArcGIS.Desktop.Core.Geoprocessing;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
 
 namespace uic_addin.Controls {
     internal class AuthorizationAction : Button {
         protected override async void OnClick() => await ThreadService.RunOnBackground(async () => {
             Log.Debug("Running Authorization missing Action Validation");
+
+            var progressDialog = new ProgressDialog("Running Tool", "Cancel", 100, false);
+            progressDialog.Show();
 
             var table = MapView.Active.Map.StandaloneTables.FirstOrDefault(x => string.Equals(
                     x.GetTable().GetName().SplitAndTakeLast('.'),
@@ -25,75 +29,51 @@ namespace uic_addin.Controls {
             if (table == null) {
                 Log.Warning("Could not find UICAuthorizationAction!");
 
-                ThreadService.RunOnUiThread(() => {
-                    var message = "🔍 The UICAuthorizationAction table could not be found. " +
-                                "Please add it to your map.";
-
-                    Log.Verbose("Showing notification: {message}", message);
-
-                    var selection = new Notification {
-                        Message = message,
-                        ImageUrl = "",
-                        Title = "UIC Add-in"
-                    };
-
-                    FrameworkApplication.AddNotification(selection);
-                });
+                NotificationService.Notify("🔍 The UICAuthorizationAction table could not be found. " +
+                                "Please add it to your map.");
 
                 return;
             }
 
             Log.Verbose("Selecting Authorizations");
 
-            var parameters = Geoprocessing.MakeValueArray(table, "NEW_SELECTION", "Authorization_FK is null");
             IGPResult result = null;
+            var parameters = Geoprocessing.MakeValueArray(table, "NEW_SELECTION", "Authorization_FK IS NULL");
+            var progSrc = new CancelableProgressorSource(progressDialog);
+
             try {
                result = await Geoprocessing.ExecuteToolAsync(
                     "management.SelectLayerByAttribute",
-                    parameters
+                    parameters,
+                    null,
+                    new CancelableProgressorSource(progressDialog).Progressor,
+                    GPExecuteToolFlags.Default
                );
-            } catch(Exception e) {
+            } catch(Exception ex) {
+                Log.Error(ex, "Select layer by attribute {@parameters}", parameters);
+
+                NotificationService.Notify("The tool crashed");
+
+                progressDialog.Hide();
+
                 return;
             }
 
             var problems = Convert.ToInt32(result?.Values[1]);
 
             if (problems == 0) {
-                ThreadService.RunOnUiThread(() => {
-                    var message = "🚀 Every Authorization has an Action record 🚀";
-
-                    Log.Verbose("Showing notification: {message}", message);
-
-                    var selection = new Notification {
-                        Message = message,
-                        ImageUrl = "",
-                        Title = "UIC Add-in"
-                    };
-
-                    FrameworkApplication.AddNotification(selection);
-                });
+                NotificationService.Notify("🚀 Every Authorization has an Action record 🚀");
 
                 return;
             }
 
+            NotificationService.Notify($"There are {problems} Authorizations with no AuthorizationAction" +
+                   "They problem authorizations have been selected.");
+
             Log.Verbose("Zooming to seleted");
 
-            MapView.Active.ZoomToSelectedAsync(TimeSpan.FromSeconds(3));
-
-            ThreadService.RunOnUiThread(() => {
-                var message = $"There are {problems} Authorizations with no AuthorizationAction" +
-                               "They problem authorizations have been selected.";
-
-                Log.Verbose("Showing notification: {message}", message);
-
-                var selection = new Notification {
-                    Message = message,
-                    ImageUrl = "",
-                    Title = "UIC Add-in"
-                };
-
-                FrameworkApplication.AddNotification(selection);
-            });
+            await MapView.Active.ZoomToSelectedAsync(TimeSpan.FromSeconds(3));
+            progressDialog.Hide();
 
             Log.Debug("Finished Authorization Validation");
         });
