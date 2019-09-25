785058fa23f5b1d1089bac5854d54017c7f6c2f9
steveoh <sgourley@utah.gov>
2019-09-25 16:26:04 -0600

    feat(authorization): add validation for no facility
    closes #36

--- Controls/AuthorizationValidations.cs
+++ Controls/AuthorizationValidations.cs
@@ -5,7 +5,6 @@ using Serilog;
 using ArcGIS.Desktop.Mapping;
 using ArcGIS.Desktop.Core.Geoprocessing;
 using ArcGIS.Desktop.Framework.Threading.Tasks;
-using System.Linq;
 
 namespace uic_addin.Controls {
     internal class AuthorizationAction : Button {
@@ -75,6 +74,76 @@ namespace uic_addin.Controls {
             await MapView.Active.ZoomToSelectedAsync(TimeSpan.FromSeconds(1.5));
 
             Log.Debug("Finished Authorization Validation");
+        });
+    }
+
+    internal class AuthorizationMissingFacilityFk : Button {
+        protected override async void OnClick() => await ThreadService.RunOnBackground(async () => {
+            Log.Debug("running authorization missing action validation");
+
+            var progressDialog = new ProgressDialog("🔍 Finding issues...", "Cancel", 100, false);
+            progressDialog.Show();
+
+            const string layerName = "UICAuthorization";
+            var layer = LayerService.GetStandaloneTable(layerName, MapView.Active.Map);
+
+            if (layer == null) {
+                NotificationService.NotifyOfMissingLayer(layerName);
+
+                progressDialog.Hide();
+
+                return;
+            }
+
+            IGPResult result = null;
+            var parameters = Geoprocessing.MakeValueArray(layer, "NEW_SELECTION", "Facility_FK IS NULL");
+            var progSrc = new CancelableProgressorSource(progressDialog);
+
+            Log.Verbose("management.SelectLayerByAttribute on {layer} with {@params}", layerName, parameters);
+
+            try {
+                result = await Geoprocessing.ExecuteToolAsync(
+                     "management.SelectLayerByAttribute",
+                     parameters,
+                     null,
+                     new CancelableProgressorSource(progressDialog).Progressor,
+                     GPExecuteToolFlags.Default
+                );
+            } catch (Exception ex) {
+                NotificationService.NotifyOfGpCrash(ex, parameters);
+
+                progressDialog.Hide();
+
+                return;
+            }
+
+            if (result.IsFailed || string.IsNullOrEmpty(result?.ReturnValue)) {
+                NotificationService.NotifyOfGpFailure(result, parameters);
+
+                progressDialog.Hide();
+
+                return;
+            }
+
+            var problems = Convert.ToInt32(result?.Values[1]);
+
+            if (problems == 0) {
+                NotificationService.NotifyOfValidationSuccess();
+
+                progressDialog.Hide();
+
+                return;
+            }
+
+            progressDialog.Hide();
+
+            NotificationService.NotifyOfValidationFailure(problems);
+
+            Log.Verbose("Zooming to selected");
+
+            await MapView.Active.ZoomToSelectedAsync(TimeSpan.FromSeconds(1.5));
+
+            Log.Debug("finished authorization missing facility fk validation");
         });
     }
 }
